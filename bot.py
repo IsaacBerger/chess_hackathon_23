62425f858b051084d30e4d2a39afd3ac5a2f5890
Isaac Berger <ikepiberger@gmail.com>
2023-11-12 21:53:37 -0500

    Adding contest submission
    This is Isaac, Ben & Brandon's submission

--- bot.py
+++ bot.py
@@ -17,13 +17,14 @@ official website or GitHub repository: https://github.com/EphraimJZimmerman/ches
 License:
 This code is open-source and released under the MIT License. See the LICENSE file for details.
 """
-
 import random
 import chess
 import time
-from collections.abc import Iterator
+from typing import Iterator
 from contextlib import contextmanager
 import test_bot
+import numpy as np
+import requests
 
 
 @contextmanager
@@ -72,14 +73,126 @@ class Bot:
         # Assume that you are playing an arbitrary game. This function, which is
         # the core "brain" of the bot, should return the next move in any circumstance.
 
-        move = str(random.choice([_ for _ in self.board.legal_moves]))
-        print("My move: " + move)
-        return move
+        # Gives a pre-set first move, a "book opening"
+        if self.board.fen() == "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1":
+            return "d2d4"
+        if self.board.fen() == "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1":
+            return "e7e5"
+        if self.board.fen() == "rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq - 0 1":
+            return "d7d5"
 
+        possible_moves = [_ for _ in self.board.legal_moves]
+        best_move = [[], -1000]
+        evaluation = 0
+
+        # looks at the evaluation of the position following each move
+        for move in possible_moves:
+            new_Board = self.board.copy()
+            new_Board.push(move)
+
+            # depth is set to 4.5
+            evaluation = self.eval_position(new_Board, 4.5)
+
+            # makes clack player play for black's advantage
+            if self.board.turn == chess.BLACK:
+                evaluation *= -1
+
+            # finds best move
+            if evaluation > best_move[1]:
+                best_move = [[move], evaluation]
+            elif evaluation == best_move[1]:
+                best_move[0].append(move)
+
+        # randomizes across equally good moves
+        move = random.choice(best_move[0])
+        print("My move: " + move.uci())
+        return move.uci()
+
+    # gives a 'score' for each position on how good it is for white
+    def eval_position(self, position, depth=0.0):
+
+        # prioritizes checkmate
+        if position.is_checkmate():
+            if position.turn == chess.WHITE:
+                return -100
+            else:
+                return 100
+        if position.is_insufficient_material():
+            return 0
+        if position.is_stalemate():
+            return 0
+
+        # base case
+        if depth <= 0:
+            # evaluates material advantage
+            white_material = 0
+            black_material = 0
+            piece_values = {'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'p': -1, 'n': -3, 'b': -3, 'r': -5, 'q': -9}
+            board_full = position.fen()
+            board = board_full[0:board_full.index(' ')]
+            rank = 8
+            for i in board:
+                if i in piece_values.keys():
+                    if piece_values[i] > 0:
+                        white_material += piece_values[i]
+
+                        # gives extra points for pushing pawns
+                        if piece_values[i] == 1:
+                            white_material += ((.1 * rank) - .45)
+                    else:
+                        black_material -= piece_values[i]
+                        if piece_values[i] == 1:
+                            black_material += (.45 - (.1 * rank))
+
+                if i == "/":
+                    rank -= 1
+
+            evaluation = np.sqrt(white_material) - np.sqrt(black_material)
+
+            return evaluation
+
+        else:
+            # lists the three different types of moves
+            boring_moves = []
+            checks = []
+            captures = []
+            for move in position.legal_moves:
+                if position.gives_check(move):
+                    checks.append(move)
+                elif position.is_capture(move):
+                    captures.append(move)
+                else:
+                    boring_moves.append(move)
+
+            # determines how far to look based on move type
+            evaluations = []
+            for move in checks:
+                new_board = position.copy()
+                new_board.push(move)
+                evaluations.append(self.eval_position(new_board, depth - .5))
+            for move in captures:
+                new_board = position.copy()
+                new_board.push(move)
+                evaluations.append(self.eval_position(new_board, depth - 2))
+
+            if depth > 2:
+                for move in boring_moves:
+                    new_board = position.copy()
+                    new_board.push(move)
+                    evaluations.append(self.eval_position(new_board, depth - 3))
+            else:
+                evaluations.append(self.eval_position(position, -1))
+
+            # assumes player will always make their best available move
+            if position.turn == chess.WHITE:
+                return np.max(evaluations)
+            else:
+                return np.min(evaluations)
 
 # Add promotion stuff
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+if True:
 
     chess_bot = Bot()  # you can enter a FEN here, like Bot("...")
     with game_manager():
